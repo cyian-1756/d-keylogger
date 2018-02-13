a227055b3ae94f088165f1ae57f1aa9b7fdfa051
cyian-1756 <devnull64@vfemail.net>
2018-02-13 07:33:27 -0500

    Added more special keycodes

--- source/app.d
+++ source/app.d
@@ -44,6 +44,12 @@ void main() {
 				}
 				debug {
 					writefln("Key code: %d", cast(int)ks);
+					if (pressedkey != null) {
+						writefln("Pressed key: %s", pressedkey);
+					} else {
+                		buf[len]=0;
+                		writefln("Pressed key: %s", buf[0]);
+					}
 				}
 			default: break;
         }
@@ -56,17 +62,26 @@ void logKey(void[] toWrite) {
 
 string getKey(int key) {
 	switch (key) {
+		case 32: return "[space]";
 		case 65288: return "[backspace]";
+		case 65289: return "[tab]";
 		case 65293: return "[enter]";
-		case 65506: return "[rshift]";
-		case 65505: return "[lshift]";
+		case 65360: return "[home]";
 		case 65361: return "[larrow]";
-		case 65364: return "[darrow]";
-		case 65363: return "[rarrow]";
 		case 65362: return "[uarrow]";
-		case 65509: return "[capslock]";
+		case 65363: return "[rarrow]";
+		case 65364: return "[darrow]";
+		case 65365: return "[pageup]";
+		case 65379: return "[insert]";
+		case 65407: return "[numlock]";
+		case 65505: return "[lshift]";
+		case 65506: return "[rshift]";
 		case 65507: return "[lctrl]";
 		case 65508: return "[rctrl]";
+		case 65509: return "[capslock]";
+		case 65513: return "[lalt]";
+		case 65514: return "[ralt]";
+		case 65535: return "[delete]";
 		default: return null;
 	}
 }
